5ed54b18a9c3297d2770b93e5efa2e0b3a50b04a
Charles Lowell <cowboyd@frontside.io>
2018-03-24 08:17:43 -0600

    WIP - Associate a value and state with every node in the structure
    There are two types of nodes in the tree, `PrimaryValue` nodes, and
    `NestedValue` nodes. Primary Value nodes have their own value, and
    they don't derive them from anywhere in the tree. NestedValue nodes
    have a `root` value which the then derive their value by lensing them
    out of the root.
    
    Big changes are that now, when creating a transition context, we need
    to update the root value of all of the nested nodes to be the local
    value at the transition site, and then once the transition is
    complete, update all the nested nodes to have their root be the new
    global value.

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ export const { create } = Microstate;
 
 export { default as Microstate } from './microstate';
 export { default as Tree } from './utils/tree';
+export { reveal } from './utils/secret';
 export { default as analyze } from './structure';
 export { parameterized } from './types/parameters.js';
